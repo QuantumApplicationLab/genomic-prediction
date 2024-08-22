edcb8da8d83d1a1c7b7c4e578f4b27e0308e3260
juanboschero <juanboschero1998@gmail.com>
2024-08-22 16:47:27 +0200

    Add test_qi_no_approx_no_X

--- tests/test_quantum_inspired.py
+++ tests/test_quantum_inspired.py
@@ -143,25 +143,24 @@ def test_cg():
     )
     assert n_matches == top_size_x
 
-
-def test_cg_low_rank():
-    """Test CG with low-rank genotype data."""
-    _, _, x_sol, ebv, y, W, Z, X, _, top_size_x, top_size_ebv = _load_data()
-    A, b = _construct_A_b(W, Z, X, y, rank_Z=80)
+def test_cg_low_rank_ignore_X():
+    """Test CG with low-rank genotype data while ignoring fixed effects."""
+    _, _, x_sol, ebv, y, W, Z, _, _, top_size_x, top_size_ebv = _load_data()
+    y = y - np.mean(y)  # simple compensation of fixed effects
+    A, b = _construct_A_b_no_X(W, Z, y, rank_Z=80)
     P = np.diag(np.diag(A))
     x_cg, _ = cg(A, b, M=P, atol=1e-5)
 
     x_idx = _find_top_indices(np.abs(x_cg), top_size_x)
-    n_matches = plot_solution(x_sol, x_idx, "test_cg_low_rank_x")
-    assert n_matches == 9
+    n_matches = plot_solution(x_sol, x_idx, "test_cg_low_rank_ignore_X_x")
+    assert n_matches == 1
 
-    ebv_cg = Z @ x_cg[X.shape[1] :]
+    ebv_cg = Z @ x_cg
     ebv_idx = _find_top_indices(np.abs(ebv_cg), top_size_ebv)
-    n_matches = plot_solution(ebv, ebv_idx, "test_cg_low_rank_ebv", expected_solution=ebv, solution=ebv_cg)
-    assert n_matches == 31
+    n_matches = plot_solution(ebv, ebv_idx, "test_cg_low_rank_ignore_X_ebv", expected_solution=ebv, solution=ebv_cg)
+    assert n_matches == 28
 
-
-def test_cg_low_rank_ignore_X():
+def test_cg_ignore_X():
     """Test CG with low-rank genotype data while ignoring fixed effects."""
     _, _, x_sol, ebv, y, W, Z, _, _, top_size_x, top_size_ebv = _load_data()
     y = y - np.mean(y)  # simple compensation of fixed effects
@@ -326,12 +325,13 @@ def test_least_squares_ignore_X():
         ("ridge"),
     ],
 )
+
 def test_qi_no_X(method: str):
     """Test quantum-inspired regression and no fixed effects."""
     # Load data
     _, _, x_sol, _, _, W, Z, X, _, _, top_size_ebv = _load_data()
-    rank = 3
-
+    rank = 3    
+    
     # Simulate specific rank for Z
     Z = _get_low_rank_approx(Z, rank)
 
@@ -392,9 +392,77 @@ def test_qi_no_X(method: str):
     else:
         assert False
 
+def test_qi_no_approx_no_X(method: str):
+    """Test quantum-inspired regression and no fixed effects."""
+    # Load data
+    _, _, x_sol, _, _, W, Z, X, _, _, top_size_ebv = _load_data()
+    # Simulate `ebv` based on a previous solution
+    ebv = np.squeeze(Z @ x_sol[X.shape[1] :])  # use `x_sol` for convenience
+    
+    Z = Z @ np.identity(Z.shape[1]) # necessary for Dataset conversion
+
+    rank = 233
+    # Leave out non-phenotyped animals
+    WZ = W @ Z
+    
+    # Simulate direct observations
+    y = ebv[WZ.shape[0] :]
+
+    # Solve using quantum-inspired algorithm
+    r = 1000
+    c = 500
+    n_samples = 100
+    print(f"n_samples: {n_samples} out of {WZ.shape[0] * WZ.shape[1]}")
+    n_entries_b = 1000
+    func = None
+    if method == "ridge":
+        var_e = 0.7
+        var_g = 0.3 / Z.shape[1] / 0.5
+        func = lambda arg: (arg**2 + var_e / var_g) / arg
+    random_state = 2
+    print(f"\nrandom_state: {random_state}")
+    rng = np.random.RandomState(random_state)
+
+    # Solve
+    qi = QILinearEstimator(r, c, rank, n_samples, rng, func=func)
+    qi = qi.fit(WZ, y)
+    
+    sampled_indices, sampled_ebv = qi.predict_b(Z, n_entries_b)
+
+    # Find most frequent outcomes
+    unique_ebv_idx, counts = np.unique(sampled_indices, return_counts=True)
+    sort_idx = np.flip(np.argsort(counts))
+    ebv_idx = unique_ebv_idx[sort_idx][:top_size_ebv]
+
+    # Compare results
+    df = pd.DataFrame({"ebv_idx_samples": sampled_indices, "ebv_samples": sampled_ebv})
+    df_mean = df.groupby("ebv_idx_samples")["ebv_samples"].mean()
+    df_counts = df.groupby("ebv_idx_samples").count()
+    unique_sampled_indices = df_mean.keys()
+    unique_sampled_ebv = np.asarray(df_mean.values)
+    n_matches = plot_solution(
+        ebv,
+        ebv_idx,
+        f"{random_state}_test_qi_{method}_no_X_{n_samples}",
+        expected_solution=_normalize(ebv[unique_sampled_indices]),
+        solution=_normalize(unique_sampled_ebv),
+        expected_counts=n_entries_b * np.abs(ebv / norm(ebv))[unique_sampled_indices] ** 2,
+        counts=np.squeeze(np.round(df_counts.values)),
+    )
+
+    print(n_matches)
+
+    if method == "ridge":
+        assert n_matches == 2
+    elif method == "ordinary":
+        assert n_matches == 2
+    else:
+        assert False
+
 
 if __name__ == "__main__":
     test_qi_no_X("ordinary")
     test_qi_no_X("ridge")
     test_cg_saturated_rank_ignore_X()
-
+    test_qi_no_approx_no_X("ordinary")
+    test_qi_no_approx_no_X("ridge")
